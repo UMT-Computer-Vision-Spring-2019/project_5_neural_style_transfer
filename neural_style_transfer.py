1b8f756bf86b8b179543d37e3a31fe68c06fb492
Kyle Lucke <luckek1354@gmail.com>
2019-04-24 16:13:16 -0600

    Everything up to prior loss is implemented

--- neural_style_transfer.py
+++ neural_style_transfer.py
@@ -14,39 +14,58 @@ import sys
 
 def content_layer_loss(Fp, Fx):
 
-    _, h, w, d = Fp.get_shape()
+    _, h, w, d = Fp.get_shape().as_list()
 
     # Compute the residuals
-    res = (Fx - Fp)
+    res = Fx - Fp
 
-    # Compute the residuals
-    sq_res = np.square(res)
+    # Square the residuals
+    sq_res = res**2
 
-    sum_sq_res = np.sum(sq_res)
+    # Compute sum of residual squares
+    sum_sq_res = K.tf.reduce_sum(sq_res)
 
+    # Note: to access underlying value: w.value
     M = w * h
     N = d
-    scale = 1.0 / ((2 * (M**0.5)) * (N**0.5))
+    scale = 1.0 / (2 * (M**0.5) * (N**0.5))
 
-    loss = scale * sum_sq_res
+    loss = sum_sq_res * scale
 
     return loss
 
 
-def gram_matrix(f, M, N):
+def gram_matrix(f):
+
+    _, h, w, d = f.get_shape().as_list()
+
+    M = h * w
+    N = d
+
     # Accepts a (height,width,depth)-sized feature map,
     # reshapes to (M,N), then computes the inner product
-    # !Change me
+    f = K.tf.reshape(f, shape=(M, N))
 
-    f = np.reshape(f, newshape=(M, N))
-
-    return np.dot(f.T, f)
+    return K.tf.tensordot(K.tf.transpose(f), f, 1)
 
 
 def style_layer_loss(Fa, Fx):
     # ! Change me
-    _, h, w, d = Fa.get_shape()
-    loss = 0
+    _, h, w, d = Fa.get_shape().as_list()
+
+    G_Fa = gram_matrix(Fa)
+    G_Fx = gram_matrix(Fx)
+
+    res = G_Fa - G_Fx
+    sq_res = res**2
+    sum_sq_res = K.tf.reduce_sum(sq_res)
+
+    M = w * h
+    N = d
+    scale = 1 / (4 * M**2 * N**2)
+
+    loss = scale * sum_sq_res
+
     return loss
 
 
@@ -146,38 +165,40 @@ blend_model = km.Model(inputs=blend_base_model.inputs, outputs=blend_outputs)
 blend_content_outputs = [blend_model.outputs[0]]
 blend_style_outputs = blend_model.outputs[1:]
 
-# content_loss = content_layer_loss(content_model.output, blend_content_outputs[0])
-#
-# # The correct output of this function is 195710720.0
-# np.random.seed(0)
-# input_img = np.random.randn(1, img_rows, img_cols, 3)
-# content_loss_evaluator = K.function([blend_model.input], [content_loss])
-# content_loss_evaluator([input_img])
-#
-# # For a correctly implemented gram_matrix, the following code will produce 113934860.0
-# fmap = content_model.output
-# _, h, w, d = fmap.get_shape()
-# M = h * w
-# N = d
-# gram_matrix_evaluator = K.function([content_model.input], [gram_matrix(fmap, M, N)])
-# gram_matrix_evaluator([content_img])[0].mean()
-#
-# style_loss_0 = style_layer_loss(style_model.output[0],blend_style_outputs[0])
-#
-# # The correct output of this function is 220990.31
-# np.random.seed(0)
-# input_img = np.random.randn(1,img_rows,img_cols,3)
-# style_loss_evaluator = K.function([blend_model.input],[style_loss_0])
-# style_loss_evaluator([input_img])
+content_loss = content_layer_loss(content_model.output, blend_content_outputs[0])
 
-# style_loss = 0
-# for i in range(5):
-#     style_loss += 0.2 * style_layer_loss(style_model.output[i], blend_style_outputs[i])
-#
+# The correct output of this function is 195710720.0
+np.random.seed(0)
+input_img = np.random.randn(1, img_rows, img_cols, 3)
+content_loss_evaluator = K.function([blend_model.input], [content_loss])
+content_loss_evaluator([input_img])
+print("Content loss:", content_loss_evaluator([input_img]))
+
+# For a correctly implemented gram_matrix, the following code will produce 113934860.0
+fmap = content_model.output
+
+gram_matrix_evaluator = K.function([content_model.input], [gram_matrix(fmap)])
+print("Gram matrix mean:", gram_matrix_evaluator([content_img])[0].mean())
+
+style_loss_0 = style_layer_loss(style_model.output[0],blend_style_outputs[0])
+
+# The correct output of this function is 220990.31
+np.random.seed(0)
+input_img = np.random.randn(1, img_rows, img_cols, 3)
+style_loss_evaluator = K.function([blend_model.input], [style_loss_0])
+print("Single layer style loss:", style_loss_evaluator([input_img]))
+
+style_loss = 0
+for i in range(5):
+    style_loss += 0.2 * style_layer_loss(style_model.output[i], blend_style_outputs[i])
+
 # The correct output of this function is 177059700.0
-# np.random.seed(0)
-# input_img = np.random.randn(1, img_rows, img_cols, 3)
-# style_loss_evaluator = K.function([blend_model.input], [style_loss])
-# style_loss_evaluator([input_img])
+np.random.seed(0)
+input_img = np.random.randn(1, img_rows, img_cols, 3)
+style_loss_evaluator = K.function([blend_model.input], [style_loss])
+print("All layer style loss:", style_loss_evaluator([input_img]))
+
+## All code up to this point works ##
+
 
 
